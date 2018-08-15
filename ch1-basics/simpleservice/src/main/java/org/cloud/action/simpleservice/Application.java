696751ad2f48e6af62485c302560571c6cebadc7
maksym <maksymus@gmail.com>
2018-08-14 21:43:28 -0400

    spring cloud: chapter1 - Basics

---
+++ ch1-basics/simpleservice/src/main/java/org/cloud/action/simpleservice/Application.java
@@ -0,0 +1,23 @@
+package org.cloud.action.simpleservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+@RequestMapping("hello")
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+    @RequestMapping(value = "/{firstName}/{lastName}", method = RequestMethod.GET)
+    public String hello(@PathVariable("firstName") String firstName,
+                        @PathVariable("lastName") String lastName) {
+        return "Hello, " + firstName + " " + lastName + "!";
+    }
+}
