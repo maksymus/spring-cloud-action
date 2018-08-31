41cba081d11eaccc9dd135eeb0a338498fee868f
maksym <maksymus@gmail.com>
2018-08-30 23:20:33 -0400

    spring cloud: chapter5 - Circuit Breaker (Hystrix) init

---
+++ ch5-circuit-breaker/license-service/src/main/java/org/cloud/action/license/Application.java
@@ -0,0 +1,24 @@
+package org.cloud.action.license;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+@EnableFeignClients
+@EnableCircuitBreaker
+@SpringBootApplication
+public class Application {
+    @LoadBalanced
+    @Bean
+    public RestTemplate getRestTemplate() {
+        return new RestTemplate();
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
