7eb27adfc7efe320319c384e614e371b19c34a17
maksym <maksymus@gmail.com>
2018-09-05 22:43:54 -0400

    spring cloud: chapter6 - Routing (Zuul) - init

---
+++ ch6-routing/license-service/src/main/java/org/cloud/action/license/config/ServiceConfig.java
@@ -0,0 +1,14 @@
+package org.cloud.action.license.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ServiceConfig{
+    @Value("${example.property}")
+    private String exampleProperty;
+
+    public String getExampleProperty(){
+        return exampleProperty;
+    }
+}
